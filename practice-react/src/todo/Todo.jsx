fcc37f4a5a9ec0fa081182aef9f734c3e3ad435c
Abhishekwmg <ak89471@gmail.com>
2026-01-15 19:34:05 +0530

    todo-checkbox

--- practice-react/src/todo/Todo.jsx
+++ practice-react/src/todo/Todo.jsx
@@ -1,83 +1,135 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Todo() {
-
-    const [data, setData] = useState(todoData);
-    const [task, setTask] = useState("");
+    const [todoData, setTodoData] = useState(preTodoData);
+    const [addTask, setAddTask] = useState("");
     const [isEdit, setIsEdit] = useState(null);
     const [saveTask, setSaveTask] = useState("");
     const [isChecked, setIsChecked] = useState(false);
 
-    function handleEditTask(d) {
-        setIsEdit(d.id);
-        setSaveTask(d.text)
+    function handleAddTask() {
+        if (!addTask) return;
+        setTodoData((prevData) => [
+            ...prevData,
+            {
+                id: Date.now(),
+                text: addTask,
+                done: false,
+            },
+        ]);
+        setAddTask("");
     }
 
-    function handleSaveTask(d) {
-        setData(data.map(da => {
-            return da.id === d.id ? { ...d, text: d.text } : d
-        }))
+    function handleEditTask(todo) {
+        setIsEdit(todo.id);
+        setSaveTask(todo.text);
+    }
+
+    function handleSaveTask(todo) {
+        setTodoData((prevData) =>
+            prevData.map((data) => {
+                return data.id === todo.id ? { ...todo, text: saveTask } : data;
+            })
+        );
         setIsEdit(null);
     }
 
-    function handleCheckBox() {
-        setIsChecked(prevChecked => !prevChecked);
+    function handleIsChecked(checked, id) {
+        setTodoData(prevData => {
+            prevData.map(data => {
+                return data.id === id ? { ...data, done: checked } : data
+            })
+        })
     }
 
     function handleDeleteTask(id) {
-        setData(data.filter(da => da.id !== id))
+        setTodoData(todoData.filter((d) => d.id != id));
     }
 
+    return (
+        <>
+            <h1>Todo List (Strict)</h1>
+            {!isEdit && (
+                <>
+                    <input
+                        value={addTask}
+                        onChange={(e) => setAddTask(e.target.value)}
+                        type="text"
+                        placeholder="Add items to list"
+                    />
+                    <button onClick={handleAddTask}>Add</button>
+                    <select name="" id="">
+                        <option value="filter">All</option>
+                        <option value="filter">Active</option>
+                        <option value="filter">Done</option>
+                    </select>
+                </>
+            )}
 
-    return <>
-        <h1>Todo App</h1>
-        <input type="text" value={task} onChange={e => setTask(e.target.value)} />
-        <button type="button">Add</button>
-
-        {/* Rendering pre-todo-list */}
-        <ol>
-            {
-                data.map((d) => {
-                    return <li key={d.id}>
-                        {
-                            isEdit === d.id ? <><input value={saveTask} onChange={e => setSaveTask(e.target.value)} type="text" />
-                                <button onClick={() => handleSaveTask(d)}>Save</button></> : <>
-                                <input type="checkbox" value={isChecked} onChange={handleCheckBox} />
-                                <p>{d.text}</p>
-                                <button onClick={() => handleEditTask(d)}>Edit</button></>
-                        }
-                        <button onClick={() => handleDeleteTask(d.id)}>Delete</button>
-                    </li>
-
-                })
-            }
-        </ol>
-
-    </>
+            <ol>
+                {todoData.map((todo) => {
+                    return (
+                        <li
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "space-between",
+                            }}
+                            key={todo.id}
+                        >
+                            {isEdit === todo.id ? (
+                                <>
+                                    <input
+                                        type="text"
+                                        value={saveTask}
+                                        onChange={(e) => setSaveTask(e.target.value)}
+                                    />
+                                    <button onClick={() => handleSaveTask(todo)}>Save</button>
+                                </>
+                            ) : (
+                                <>
+                                    <input
+                                        type="checkbox"
+                                        checked={todo.done}
+                                        onChange={() => handleIsChecked(todo.id)}
+                                    />
+                                    <p>{todo.text}</p>
+                                    <button onClick={(e) => handleEditTask(todo.id, e.target.checked)}>Edit</button>
+                                </>
+                            )}
+                            <button onClick={() => handleDeleteTask(todo.id)}>Delete</button>
+                        </li>
+                    );
+                })}
+            </ol>
+        </>
+    );
 }
 
-
-
-
-const todoData = [
+const preTodoData = [
     {
         id: 1,
-        text: "Buy Avacado",
-        done: false
+        text: "Buy Avocado",
+        done: false,
     },
     {
         id: 2,
-        text: "Visit Local Market",
-        done: false
+        text: "Get Milk",
+        done: false,
     },
     {
         id: 3,
-        text: "Get Healthy",
-        done: false
+        text: "Run Marathon",
+        done: false,
     },
     {
         id: 4,
-        text: "Cook Rice",
-        done: false
+        text: "Get Healthy",
+        done: false,
     },
-]
+    {
+        id: 5,
+        text: "Start Business",
+        done: false,
+    },
+];
